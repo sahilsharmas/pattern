6077319d45956e0ce56fb10308000a0dee8662cc
sahil <135645401+sahilsharmas@users.noreply.github.com>
2023-06-13 09:17:02 +0530

    pattern1.java

---
+++ class pattern1.java
@@ -0,0 +1,24 @@
+  class pattern1
+{
+    public  static  void main (String args[])
+    {
+        int i,j;
+        { 
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("x");
+                } System.out.println(" ");
+            }
+            
+            {
+                for(i=1;i<=5;i++)
+                {
+                    for(j=0;j>=i;j--)
+                
+                System.out.print("x");
+            }System.out.println(" ");
+        }
+    }}
+}
